f75fd3e2823e368f799c316c19e0b28eff870667
NaSinSa <andy.k.oh@hotmail.com>
2020-03-24 15:42:49 -0400

    add application test. remove simple render test

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,7 +5,7 @@ import DayListItem from "./DayListItem";
 
   export default function DayList(props) {
     const dayChecker = props.days.map((ele) => {
-      return <DayListItem id={ele.id} name={ele.name} spots={ele.spots} selected={ele.name === props.day} setDay={props.setDay} />
+      return <DayListItem key={ele.id} id={ele.id} name={ele.name} spots={ele.spots} selected={ele.name === props.day} setDay={props.setDay} />
       })
     
     return ( <ul>
--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -6,54 +6,6 @@ import Appointment from "components/Appointment";
 
 afterEach(cleanup);
 
-// const state = {
-//     day: "Monday",
-//     days: [
-//         {
-//           id: 1,
-//           name: "Monday",
-//           spots: 2,
-//         },
-//         {
-//           id: 2,
-//           name: "Tuesday",
-//           spots: 5,
-//         },
-//         {
-//           id: 3,
-//           name: "Wednesday",
-//           spots: 0,
-//         },
-//       ],
-//     appointments: [
-//       {
-//         id: 1,
-//         time: "12pm",
-//         interview: null
-//       },
-//       {
-//         id: 2,
-//         time: "1pm",
-//         interview: {
-//           student: "Lydia Miller-Jones",
-//           interviewer: {
-//             id: 1,
-//             name: "Sylvia Palmer",
-//             avatar: "https://i.imgur.com/LpaY82x.png",
-//           }
-//         }
-//       }
-//     ],
-//     interviewers: {
-//       "1": { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-//       "2": { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-//       "3": { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-//       "4": { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-//       "5": { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-//     },
-
-// }
-
 describe("Appointment", () => {
     it("renders without crashing", () => {
       render(<Appointment />);
